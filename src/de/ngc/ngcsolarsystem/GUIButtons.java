5d9b5d59b16e54fa3753daf349596477416ac1f2
philipjar <philipjar@gmail.com>
2015-11-22 19:54:49 +0100

    Added GUIButtons Jpanel for sidepanel buttons

---
+++ src/de/ngc/ngcsolarsystem/GUIButtons.java
@@ -0,0 +1,22 @@
+package de.ngc.ngcsolarsystem;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class GUIButtons extends JPanel {
+	
+	private static final long serialVersionUID = -2773637082067263110L;
+	
+	JButton button;
+	
+	public GUIButtons() {
+		setBackground(Color.GRAY);
+		button = new JButton("+");
+		button.setPreferredSize(new Dimension(40, 40));
+		add(button);
+	}
+
+}
