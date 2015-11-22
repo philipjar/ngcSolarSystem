46145c517b3258f839fdde54e747ced9a37464c1
philipjar <philipjar@gmail.com>
2015-11-22 20:37:12 +0100

    Added Zoom buttons

--- src/de/ngc/ngcsolarsystem/GUIButtons.java
+++ src/de/ngc/ngcsolarsystem/GUIButtons.java
@@ -2,6 +2,8 @@ package de.ngc.ngcsolarsystem;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Insets;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -10,13 +12,24 @@ public class GUIButtons extends JPanel {
 	
 	private static final long serialVersionUID = -2773637082067263110L;
 	
-	JButton button;
+	JButton buttonZoomIn;
+	JButton buttonZoomOut;
 	
 	public GUIButtons() {
 		setBackground(Color.DARK_GRAY);
-		button = new JButton("+");
-		button.setPreferredSize(new Dimension(40, 40));
-		add(button);
+		setLayout(new FlowLayout());
+		
+		buttonZoomIn = new JButton();
+		buttonZoomIn.setText("+");
+		buttonZoomIn.setPreferredSize(new Dimension(40, 40));
+		buttonZoomIn.setMargin(new Insets(0, 0, 0, 0));
+		add(buttonZoomIn);
+		
+		buttonZoomOut = new JButton();
+		buttonZoomOut.setText("-");
+		buttonZoomOut.setPreferredSize(new Dimension(40, 40));
+		buttonZoomOut.setMargin(new Insets(0, 0, 0, 0));
+		add(buttonZoomOut);
 	}
 
 }
